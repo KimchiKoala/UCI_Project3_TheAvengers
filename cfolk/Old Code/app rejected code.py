697dcbacb1d9fb83385e737099ae7f07f7f98212
cassfolk <cassandra.folkers@gmail.com>
2021-03-03 19:02:42 -0800

    slight chages

---
+++ cfolk/Old Code/app rejected code.py
@@ -0,0 +1,70 @@
+# @app.route('/stock', methods = ['GET', 'POST'])
+# def form():
+#     if request.method == 'POST':
+#         stock = request.form['stock']
+#         print(stock)
+#     return render_template('index.html', stock=stock)
+
+            # @app.route('/', methods = ['POST'])
+            # def get_stock():
+            #     # stock = request.form['stock']
+            #     # processed_stock = stock.upper()
+            #     # return processed_stock
+            #     form_value = request.args.get('stock')
+            #     return print(form_value)
+
+
+# @app.route('/', methods = ['GET', 'POST'])
+# def stock():
+
+#     if 'stock' in session:
+#         stock = session['stock']
+
+#     if request.method == "POST": 
+#         session['stock'] = request.form['stockInput']
+#     return print(stock)
+
+
+#     stock = request.form['stockInput']
+#     # print(stock)
+#     return stock
+#     # stock = None
+
+
+# #     return render_template('index.html', stock=stock)
+
+                # @app.route('/', methods = ['GET', 'POST'])
+                # def index():
+                #     if 'stock' in session:
+                #         stock = session['stock']
+
+
+                #     if request.method == "POST": 
+                #         stock = request.form['stockInput']
+                #         print(stock)
+                #     #     # return print(session['stock'])
+
+                #     # if request.method == 'POST':
+                #     #     stock = request.form
+                #     #     print(stock)
+
+                #     return render_template('index.html')
+
+
+            # @app.route('/read-one')
+            # def read_one():
+            #     # filt = {'symbol': stock}
+            #     # # filt = {'symbol' : 'GOOGL'}
+            #     # user = mongo.db.stock_data.find_one(filt)
+            #     # output = {'historical' : user['historical']}
+            #     # return output
+            #     return print(stock)
+
+            # @app.route('/all')
+            # def read_all():
+            #     # users = mongo.db.stock_data.find()
+            #     # output = {'symbol': users['symbol']}
+            #     # return jsonify(output)
+
+
+            # # , 'historical' : users['historical']
